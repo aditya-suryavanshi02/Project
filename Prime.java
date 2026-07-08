45c0c4a48741767b3654add37c29d7f7fd3a641b
Aditya Suryavanshi <adityasuryavanshi7273@gmail.com>
2026-07-08 12:44:55 +0530

    Print Prime number.

---
+++ Prime.java
@@ -0,0 +1,25 @@
+public class Prime {
+    public static void main(String[] args) {
+        int n = 20; // Specify the range up to which you want to print prime numbers
+
+        System.out.println("Prime numbers from 1 to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
